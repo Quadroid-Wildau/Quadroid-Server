aa7b94e5f9bba1c865ce406ac1ad02d1ae251ff1
Sascha Hillig <sascha@wonderweblabs.io>
2013-10-29 14:21:54 +0100

    initialized rspec structure

---
+++ spec/requests/api/v1/landmark_alerts_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe 'Landmark alerts API' do
+
+  describe 'index action' do
+
+  end
+
+  describe 'show action' do
+
+  end
+
+  describe 'create action' do
+
+    # post :photo, :file => Rack::Test::UploadedFile.new(path, mime_type) # text/jpg
+
+  end
+
+  describe 'update action' do
+
+  end
+
+  describe 'destroy action' do
+
+  end
+
+
+  # it 'xxx' do
+  #   FactoryGirl.create_list(:message, 10)
+
+  #   get '/api/v1/messages'
+
+  #   expect(response).to be_success            # test for the 200 status-code
+  #   json = JSON.parse(response.body)
+  #   expect(json['messages'].length).to eq(10) # check to make sure the right amount of messages are returned
+  # end
+
+end
