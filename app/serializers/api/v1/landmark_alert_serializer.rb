427fa09b1487908947036f8fc425658b8e6840fa
Sascha Hillig <sascha@wonderweblabs.io>
2014-01-02 23:27:20 +0100

    dummy data for landmark alert image

--- app/serializers/api/v1/landmark_alert_serializer.rb
+++ app/serializers/api/v1/landmark_alert_serializer.rb
@@ -1,6 +1,10 @@
 class API::V1::LandmarkAlertSerializer < ActiveModel::Serializer
   attributes :id, :latitude, :longitude, :height, :detection_date, :image_url
 
+  def image_url
+    "http://placekitten.com/g/200/300"
+  end
+
   def detection_date
     object.detection_date.to_i
   end
