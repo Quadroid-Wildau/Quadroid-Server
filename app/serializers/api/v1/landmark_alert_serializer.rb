008d62572af9bba055cd59d82c96af9f3aa2535b
Sascha Hillig <sascha@wonderweblabs.io>
2014-01-04 12:22:17 +0100

    changed image url loading

--- app/serializers/api/v1/landmark_alert_serializer.rb
+++ app/serializers/api/v1/landmark_alert_serializer.rb
@@ -2,7 +2,9 @@ class API::V1::LandmarkAlertSerializer < ActiveModel::Serializer
   attributes :id, :latitude, :longitude, :height, :detection_date, :image_path
 
   def image_path
-    "http://placekitten.com/g/200/300"
+    # object.image.url
+    URI.join(request.url, object.image.url)
+    # "http://placekitten.com/g/200/300"
   end
 
   def detection_date
