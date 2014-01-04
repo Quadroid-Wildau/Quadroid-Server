6254519d4bf3b40fd7f51f4e5fe97756fb64a99c
Sascha Hillig <sascha@wonderweblabs.io>
2014-01-04 13:46:51 +0100

    added env for staging and fixed image url issue

--- app/models/landmark_alert.rb
+++ app/models/landmark_alert.rb
@@ -6,7 +6,7 @@ class LandmarkAlert < ActiveRecord::Base
   # image file
   has_attached_file :image,
     path: "public/files/images/landmarks/:id/:style.:extension",
-    url: "/files/images/:id/:style.:extension",
+    url: "/files/images/landmarks/:id/:style.:extension",
     styles: {
       thumb: '200x200#'
     },
