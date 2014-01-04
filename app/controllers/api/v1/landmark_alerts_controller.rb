4317a8d9284ab9a5f817f17a777ba2345f118a9b
Sascha Hillig <sascha@wonderweblabs.io>
2014-01-04 12:38:51 +0100

    changed image path generation for landmarks api

--- app/controllers/api/v1/landmark_alerts_controller.rb
+++ app/controllers/api/v1/landmark_alerts_controller.rb
@@ -4,14 +4,14 @@ class Api::V1::LandmarkAlertsController < Api::V1::BaseController
   def index
     landmarks = LandmarkAlert.order('detection_date DESC')
 
-    render json: landmarks, each_serializer: ::API::V1::LandmarkAlertSerializer, status: :ok
+    render json: landmarks, each_serializer: ::API::V1::LandmarkAlertSerializer, status: :ok, request: request
   end
 
   # GET /landmark_alerts/:id
   def show
     landmark = LandmarkAlert.find_by_id(params[:id])
     if landmark
-      render json: landmark, serializer: ::API::V1::LandmarkAlertSerializer, status: :ok
+      render json: landmark, serializer: ::API::V1::LandmarkAlertSerializer, status: :ok, request: request
     else
       render json: { error: { code: 1, message: 'Could not find landmark alert for passed id.' } }, status: :not_found
     end
--- app/serializers/api/v1/landmark_alert_serializer.rb
+++ app/serializers/api/v1/landmark_alert_serializer.rb
@@ -2,8 +2,8 @@ class API::V1::LandmarkAlertSerializer < ActiveModel::Serializer
   attributes :id, :latitude, :longitude, :height, :detection_date, :image_path
 
   def image_path
-    # object.image.url
-    URI.join(request.url, object.image.url)
+    return object.image.url unless self.options[:request]
+    URI.join(self.options[:request].url, object.image.url).to_s
     # "http://placekitten.com/g/200/300"
   end
 
